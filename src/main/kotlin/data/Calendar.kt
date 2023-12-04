8e37b58a011a00c187bb2fd1995e2aaa5d2a842d
minpie <xhxhdwkd@gmail.com>
2023-12-04 17:35:10 +0900

    23.12.04/17:34

--- src/main/kotlin/data/Calendar.kt
+++ src/main/kotlin/data/Calendar.kt
@@ -1,5 +1,7 @@
 package data
 
+import java.util.Calendar
+
 class Calendar {
     private var events:Collection<Event>
 
@@ -16,8 +18,57 @@ class Calendar {
     fun DeleteEvent(){
 
     }
-    fun PutCalendar(year:Int, month:Int){
+    fun FindEvent(year:Int, month:Int, day:Int):ArrayList<Event>{
+        var rtn: ArrayList<Event> = arrayListOf()
+        for (evnt in events){
+            val check_query = arrayOf(year, month, day)
+            val dates = evnt.GetWhen()
+            val check_target = arrayOf(dates[0], dates[1], dates[2])
+            if(check_query.equals(check_target)){
+                rtn.add(evnt)
+            }
+        }
+        return rtn
+    }
+    fun FindEvent(year:Int, month:Int, day:Int, hour: Int, minute:Int, second: Int):Event?{
+        var rtn: Event? = null
+        for (evnt in events){
+            val check_query = arrayOf(year, month, day, hour, minute, second)
+            if(check_query.equals(evnt)){
+                rtn = evnt
+                break
+            }
+        }
+        return rtn
+    }
 
+    fun PutCalendar(year:Int, month:Int):String{
+        // Variable:
+        var result:String = ""
+
+        // 초기 연산:
+        val firstDayOfWeek = 0 // 첫 날의 요일을 구함 (1: 일요일, 2: 월요일, ..., 7: 토요일) /TEST
+        val lastDayOfMonth = 0 // 해당 월의 마지막 날짜를 구함 /TEST
+
+
+        // 표 헤더 출력
+        result += "일\t\t월\t\t화\t\t수\t\t목\t\t금\t\t토"
+
+        // 첫 번째 주의 날짜까지의 빈 칸 출력
+        for(i in 1 until firstDayOfWeek)
+            result += "\t\t" // 각 날짜를 두 칸으로 늘림
+        // 날짜 & 이벤트 개수 출력하기:
+        for(day in 1..lastDayOfMonth){
+            // 날짜 출력 (빈 날짜에는 빈 칸 출력)
+            val eventCount = FindEvent(year, month, day).size // 해당 날짜의 이벤트 수 /TEST
+            result += "${day}${if (eventCount > 0) "\t(${eventCount})" else "\t"}\t"
+            // 줄바꿈 처리 (7일마다)
+            if ((firstDayOfWeek + day - 1) % 7 == 0) {
+                result += "\n"
+            }
+        }
+
+        return result
     }
 
 }
--- src/main/kotlin/data/Event.kt
+++ src/main/kotlin/data/Event.kt
@@ -2,23 +2,32 @@ package data
 
 class Event {
     // Essential Property:
+    private var when_year:Int
+    private var when_month:Int
+    private var when_day:Int
     private var when_hour:Int
     private var when_minute:Int
     private var when_second:Int
 
     // Essential Function:
-    constructor(hour: Int, minute:Int, second: Int){
+    constructor(year:Int, month:Int, day:Int, hour: Int, minute:Int, second: Int){
+        when_year = year
+        when_month = month
+        when_day = day
         when_hour = hour
         when_minute = minute
         when_second = second
     }
-    fun GetWhen(): Array<Int> = arrayOf(when_hour, when_minute, when_second)
-    fun SetWhen(hour: Int, minute: Int, second: Int){
+    fun GetWhen(): Array<Int> = arrayOf(when_year, when_month, when_day, when_hour, when_minute, when_second)
+    fun SetWhen(year:Int, month:Int, day:Int, hour: Int, minute:Int, second: Int){
+        when_year = year
+        when_month = month
+        when_day = day
         when_hour = hour
         when_minute = minute
         when_second = second
     }
     fun toText():String{
-        return "${when_hour}.${when_minute}.${when_second}"
+        return "${when_year}.${when_month}.${when_day}.${when_hour}.${when_minute}.${when_second}"
     }
 }
