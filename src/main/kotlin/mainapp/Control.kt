a1350acbee3d81444b99947dbfcd7c5501f95a03
minpie <xhxhdwkd@gmail.com>
2023-12-09 23:20:38 +0900

    Edited, 2023.12.09/23:20

--- src/main/kotlin/mainapp/Control.kt
+++ src/main/kotlin/mainapp/Control.kt
@@ -4,7 +4,6 @@ import utils.*
 import data.Calendar
 import java.io.File
 import com.google.gson.*
-import data.Event
 
 class Control {
     lateinit var current_calendar:Calendar
@@ -16,35 +15,12 @@ class Control {
         LoadCalendar(fname)
     }
     fun SaveCalendar(fname: String) {
-        val gson = Gson()
-        File(fname).writeText(gson.toJson(current_calendar))
-        /*
-
-        // 실제로 저장할 이벤트 리스트
-        val eventsToSave = current_calendar.getEvents().map {
-            val jsonEvent = JsonObject()
-            jsonEvent.addProperty("when_year", it.when_year)
-            jsonEvent.addProperty("when_month", it.when_month)
-            jsonEvent.addProperty("when_day", it.when_day)
-            jsonEvent.addProperty("when_hour", it.when_hour)
-            jsonEvent.addProperty("when_minute", it.when_minute)
-            jsonEvent.addProperty("when_second", it.when_second)
-            jsonEvent.addProperty("type", it.type)
-            jsonEvent.addProperty("title", it.title)
-            jsonEvent.addProperty("content", it.content)
-            jsonEvent
+        try {
+            val gson = Gson()
+            File(fname).writeText(gson.toJson(current_calendar))
+        } catch (e: Exception) {
+            println("Error saving calendar to file: ${e.message}")
         }
-
-        // 각 JSON 이벤트를 파일에 추가
-        eventsToSave.forEach { event ->
-            val jsonString = gson.toJson(event)
-            File(fname).appendText("$jsonString\n")
-        }
-
-        println("캘린더가 성공적으로 저장되었습니다.")
-
-         */
-
     }
     fun LoadCalendar(fname:String){
         if(!File(fname).exists()){
@@ -52,112 +28,24 @@ class Control {
         }
         val jsonString = File(fname).readText()
         current_calendar = Gson().fromJson(jsonString, Calendar::class.java)
-
-
-
-
-        /*
-        print("날짜를 입력하세요 (yyyy/mm/dd) : ")
-        val userInput = readLine()?.trim()
-
-        if (!userInput.isNullOrBlank()) {
-            val (year, month, day) = userInput.split("/").takeIf { it.size == 3 } ?: run {
-                println("잘못된 입력입니다.")
-                return
-            }
-
-            val events = jsonString.split("\n")
-
-            println("\n")
-            println("$userInput 의 이벤트 정보")
-            print("-------------------------------------------------------- \n\n")
-
-            val matchingEvents = events.filterIndexed { index, eventData ->
-                if (index % 2 == 0) {
-                    val eventInfo = parseEventInfo(eventData)
-
-                    val eventYear = eventInfo.date.substringBefore("/").toIntOrNull()
-                    val eventMonth = eventInfo.date.substringAfter("/").substringBefore("/").toIntOrNull()
-                    val eventDay = eventInfo.date.substringAfterLast("/").toIntOrNull()
-
-                    val isMatchingDate = year.toInt() == eventYear && month.toInt() == eventMonth && day.toInt() == eventDay
-
-
-                    if (isMatchingDate) {
-
-                        // 필터링된 데이터가 원본 데이터의 N번째 일때, N번째 데이터 출력
-                        val currentEventData = events[index]
-                        val currentEventInfo = parseEventInfo(currentEventData)
-                        println("[이벤트 시작 날짜] : ${currentEventInfo.date}")
-                        println("[이벤트 종료 날짜] : ${currentEventInfo.date}")
-                        println("[이벤트 제목] : ${currentEventInfo.title}")
-                        println("[이벤트 내용] : ${currentEventInfo.content} \n")
-
-                        // N+1 번째 데이터 출력
-                        if (index + 1 < events.size) {
-                            val nextEventData = events[index + 1]
-                            val nextEventInfo = parseEventInfo(nextEventData)
-                            println("[이벤트 시작 날짜] : ${nextEventInfo.date}")
-                            println("[이벤트 종료 날짜] : ${nextEventInfo.date}")
-                            println("[이벤트 제목] : ${nextEventInfo.title}")
-                            println("[이벤트 내용] : ${nextEventInfo.content}")
-                            print("-------------------------------------------------------- \n")
-                        } else {
-                            println("다음 데이터가 없습니다.")
-                        }
-                    }
-                    isMatchingDate
-                } else {
-                    false
-                }
-            }
-        }
-        */
     }
     fun CheckEvent(){
-
     }
     fun AddEvent(year:Int, month:Int, day:Int, hour: Int, minute:Int, second: Int, type:String, title:String, content:String) {
         current_calendar.AddEvent(year, month, day, hour, minute, second, type, title, content)
-
-        /*
-        println("이벤트를 추가합니다.")
-        val startEvent = createEventFromUserInput("시작")
-        println("시작 이벤트: $startEvent")
-        val endEvent = createEventFromUserInput("종료")
-        println("종료 이벤트: $endEvent")
-
-
-        current_calendar.AddEvent(
-            startEvent.when_year, startEvent.when_month, startEvent.when_day,
-            startEvent.when_hour, startEvent.when_minute, startEvent.when_second,
-            startEvent.type, startEvent.title, startEvent.content
-        )
-
-        current_calendar.AddEvent(
-            endEvent.when_year, endEvent.when_month, endEvent.when_day,
-            endEvent.when_hour, endEvent.when_minute, endEvent.when_second,
-            endEvent.type, endEvent.title, endEvent.content
-        )
-
-        println("이벤트가 추가되었습니다.")
-
-        // 이벤트 정보를 JSON 형식으로 파일에 저장
-        val gson = Gson()
-        val startJsonString = gson.toJson(startEvent)
-        val endJsonString = gson.toJson(endEvent)
-        File("event.txt").appendText("$startJsonString\n$endJsonString\n")
-        println("이벤트가 성공적으로 저장되었습니다.")
-
-         */
     }
-    fun EditEvent(){
-
-    }
-    fun DeleteEvent(){
-
+    fun DeleteEvent(year:Int, month:Int, day:Int, hour: Int, minute:Int, second: Int, type:String, title:String, content:String){
+        current_calendar.DeleteEvent(current_calendar.FindEvent(year, month, day, hour, minute, second, type, title, content))
     }
     fun ShowCalenderMonth(year:Int, month:Int):String{
         return current_calendar.PutCalendar(year, month)
     }
+    fun EditEvent(
+        year:Int, month:Int, day:Int, hour: Int, minute:Int, second: Int, type:String, title:String, content:String,
+        year2:Int, month2:Int, day2:Int, hour2: Int, minute2:Int, second2: Int, type2:String, title2:String, content2:String
+        ) {
+        current_calendar.EditEventTime(current_calendar.FindEvent(year, month, day, hour, minute, second, type, title, content), year2, month2, day2, hour2, minute2, second2)
+        current_calendar.EditEventContent(current_calendar.FindEvent(year2, month2, day2, hour2, minute2, second2, type, title, content), type2, title2, content2)
+    }
+    fun GetEvent():
 }
--- src/main/kotlin/mainapp/main.kt
+++ src/main/kotlin/mainapp/main.kt
@@ -2,9 +2,7 @@ package mainapp
 
 import utils.*
 import data.Event
-import java.util.*
-import java.io.File
-import com.google.gson.*
+
 
 
 var isRunning = true
@@ -17,7 +15,7 @@ fun main() {
 
     while (isRunning) {
         // 메뉴 출력
-        print("메뉴 번호를 선택하세요 (1: 종료, 2: 달력조회, 3: 이벤트 추가, 4: 이벤트 조회): ")
+        print("메뉴 번호를 선택하세요 (1: 종료, 2: 달력조회, 3: 이벤트 추가, 4: 제목으로 이벤트 조회 5: 내용으로 이벤트 조회): ")
         // 사용자 입력 받기
         val choice = readLine()?.trim()?.toIntOrNull() ?: continue
         // 메뉴 처리
@@ -41,15 +39,6 @@ fun main() {
                 println("이벤트 시작 시점:")
                 val startDate = ParseDateStr(GetString("년/월/일 을 입력하세요(yyyy/mm/dd):"))
                 val startTime = ParseTimeStr(GetString("시/분/초 를 입력하세요(hh/mm/ss):"))
-                /*
-                val startYear = GetNumInt("년도를 입력하세요: ")
-                val startMonth = GetNumInt("월을 입력하세요: ")
-                val startDay = GetNumInt("일을 입력하세요: ")
-                val startHour = GetNumInt("시간을 입력하세요: ")
-                val startMinute = GetNumInt("분을 입력하세요: ")
-                val startSecond = GetNumInt("초를 입력하세요: ")
-
-                 */
                 val startType = GetString("종류를 입력하세요:")
                 val startTitle = GetString("제목을 입력하세요:")
                 val startContent = GetString("내용을 입력하세요:")
@@ -57,51 +46,79 @@ fun main() {
                 println("이벤트 종료 시점:")
                 val endDate = ParseDateStr(GetString("년/월/일 을 입력하세요(yyyy/mm/dd):"))
                 val endTime = ParseTimeStr(GetString("시/분/초 를 입력하세요(hh/mm/ss):"))
-                /*
-                val endYear = GetNumInt("년도를 입력하세요: ")
-                val endMonth = GetNumInt("월을 입력하세요: ")
-                val endDay = GetNumInt("일을 입력하세요: ")
-                val endHour = GetNumInt("시간을 입력하세요: ")
-                val endMinute = GetNumInt("분을 입력하세요: ")
-                val endSecond = GetNumInt("초를 입력하세요: ")
-                */
                 val endType = GetString("종류를 입력하세요:")
                 val endTitle = GetString("제목을 입력하세요:")
                 val endContent = GetString("내용을 입력하세요:")
 
-                sess1.current_calendar.AddEvent(startDate[0], startDate[1], startDate[2], startTime[0], startTime[1], startTime[2], startType, startTitle, startContent)
-                sess1.current_calendar.AddEvent(endDate[0], endDate[1], endDate[2], endTime[0], endTime[1], endTime[2], endType, endTitle, endContent)
-
-
-
-                //sess1.current_calendar.AddEvent(startYear, startMonth, startDay, startHour, startMinute, startSecond, startType, startTitle, startContent)
-                //sess1.current_calendar.AddEvent(endYear, endMonth, endDay, endHour, endMinute, endSecond, endType, endTitle, endContent)
-
+                sess1.AddEvent(startDate[0], startDate[1], startDate[2], startTime[0], startTime[1], startTime[2], startType, startTitle, startContent)
+                sess1.AddEvent(endDate[0], endDate[1], endDate[2], endTime[0], endTime[1], endTime[2], endType, endTitle, endContent)
                 println("이벤트가 추가되었습니다.")
             }
 
             4 -> {
+                println("이벤트를 제목으로 검색합니다.")
+                val searchingInput = GetString("이벤트 제목을 입력하십시오 : ")
 
+
+
+
+
+
+                val foundEvents = sess1.current_calendar.getEvents().filter { it.GetContent()[1] == searchingInput }
+
+                printFoundEvents(foundEvents)
+            }
+
+            5 -> {
+                println("이벤트를 내용으로 검색합니다.")
+                val searchingInput = GetString("이벤트 내용을 입력하십시오 : ")
+
+
+
+
+
+                val foundEvents = sess1.current_calendar.getEvents().filter { it.GetContent()[2] == searchingInput }
+                printFoundEvents(foundEvents)
+            }
+
+            6-> {
+                // 사용자에게 수정할 이벤트 정보 입력 받기
+                val startDate = ParseDateStr(GetString("수정할 이벤트의 년/월/일을 입력하세요(yyyy/mm/dd):"))
+                val startTime = ParseTimeStr(GetString("수정할 이벤트의 시/분/초를 입력하세요(hh/mm/ss):"))
+                val eventType = GetString("수정할 이벤트의 종류를 입력하세요:")
+                val eventTitle = GetString("수정할 이벤트의 제목을 입력하세요:")
+                val eventContent = GetString("수정할 이벤트의 내용을 입력하세요:")
+
+
+                // 사용자에게 수정할 내용 입력 받기
+                val newStartDate = ParseDateStr(GetString("새로운 년/월/일을 입력하세요(yyyy/mm/dd):"))
+                val newStartTime = ParseTimeStr(GetString("새로운 시/분/초를 입력하세요(hh/mm/ss):"))
+                val newEventType = GetString("새로운 종류를 입력하세요:")
+                val newEventTitle = GetString("새로운 제목을 입력하세요:")
+                val newEventContent = GetString("새로운 내용을 입력하세요:")
+
+                // 이벤트 찾기
+                sess1.EditEvent(
+                    startDate[0], startDate[1], startDate[2], startTime[0], startTime[1], startTime[2], eventType, eventTitle, eventContent,
+                    newStartDate[0], newStartDate[1], newStartDate[2], newStartTime[0], newStartTime[1], newStartTime[2], newEventType, newEventTitle, newEventContent
+                )
             }
         }
     }
 
 }
-/*
-data class EventInfo(val date: String, val time: String, val title: String, val content: String)
 
-fun parseEventInfo(eventString: String): EventInfo {
-    val eventData = eventString.split(",")
-    val eventDate = "${eventData.getOrNull(0)?.substringAfter(":")?.trim('"')}/${eventData.getOrNull(1)?.substringAfter(":")?.trim('"')}/${eventData.getOrNull(2)?.substringAfter(":")?.trim('"')}"
 
-    // 시간 부분을 12:0:0에서 12:00:00으로 포맷팅
-    val eventTimeList = (3 until 6).map { eventData.getOrNull(it)?.substringAfter(":")?.trim('"')?.padStart(2, '0') ?: "00" }
-    val eventTime = eventTimeList.joinToString(":")
-
-    val eventTitle = eventData.getOrNull(7)?.substringAfter(":")?.trim('"') ?: ""
-    val eventContent = eventData.getOrNull(8)?.substringAfter(":")?.substringBeforeLast("\"")?.trim('"') ?: ""
-
-    return EventInfo(eventDate, eventTime, eventTitle, eventContent)
+// 검색된 이벤트 출력 함수
+fun printFoundEvents(foundEvents: List<Event>) {
+    if (foundEvents.isNotEmpty()) {
+        println("검색된 이벤트:")
+        for (event in foundEvents) {
+            val whenArray = event.GetWhen()
+            val formattedDate = "${whenArray[0]}년 ${whenArray[1]}월 ${whenArray[2]}일 ${whenArray[3]}시 ${whenArray[4]}분 ${whenArray[5]}초"
+            println("제목: ${event.GetContent()[1]}, 내용: ${event.GetContent()[2]}, 날짜: $formattedDate")
+        }
+    } else {
+        println("일치하는 이벤트가 없습니다.")
+    }
 }
-
- */
